6e4a67e14c5a42a6826b8a21cc68eb3f051b3aab
Rohit <rohityadav787898@gmail.com>
2024-05-15 20:59:26 +0545

    Creted project and different components

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import MobileNav from "@/components/shared/MobileNav";
+import SideBar from "@/components/shared/SideBar";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      <SideBar />
+      <MobileNav />
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
---
+++ constants/index.ts
@@ -0,0 +1,47 @@
+export const navLinks = [
+  {
+    label: "Home",
+    route: "/",
+    icon: "/assets/icons/home.svg",
+  },
+  {
+    label: "Dashboard",
+    route: "/transformations/add/restore",
+    icon: "/assets/icons/image.svg",
+  },
+  {
+    label: "Projects",
+    route: "/transformations/add/fill",
+    icon: "/assets/icons/stars.svg",
+  },
+  {
+    label: "Tasks",
+    route: "/transformations/add/remove",
+    icon: "/assets/icons/scan.svg",
+  },
+  {
+    label: "Reporting",
+    route: "/transformations/add/recolor",
+    icon: "/assets/icons/filter.svg",
+  },
+  {
+    label: "Users",
+    route: "/transformations/add/removeBackground",
+    icon: "/assets/icons/camera.svg",
+  },
+  {
+    label: "Support",
+    route: "/profile",
+    icon: "/assets/icons/profile.svg",
+  },
+  {
+    label: "Settings",
+    route: "/credits",
+    icon: "/assets/icons/bag.svg",
+  },
+  {
+    label: "Profile",
+    route: "/credits",
+    icon: "/assets/icons/bag.svg",
+  },
+];
